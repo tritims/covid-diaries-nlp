cada6c9f544c8f1b57f6ea62f74ac0c419e98ebf
tritims <tritims@gmail.com>
2021-05-04 14:57:50 +0530

    Exception Handling

--- covid_diaries_nlp.py
+++ covid_diaries_nlp.py
@@ -1,13 +1,13 @@
 import pke
 import json
 import requests
-import nltk
+# import modules
 import text2emotion as te
 from profanity_check import predict, predict_prob
 from flashtext import KeywordProcessor
 from collections import Counter
 
-nltk.download('stopwords')
+# nltk.download('stopwords')
 # Prepare keyword processor to extract cities list
 cities_list = []
 with open('cities.json', 'r') as j:
@@ -30,34 +30,46 @@ class CovidDiariesNLP:
         }
     
     def __getKeywords__(self, doc):
-        # initialize keyphrase extraction model, here TopicRank
-        extractor = pke.unsupervised.TopicRank()
+        try:
+            # initialize keyphrase extraction model, here TopicRank
+            extractor = pke.unsupervised.TopicRank()
 
-        # load the content of the document, here document is expected to be in raw
-        # format (i.e. a simple text file) and preprocessing is carried out using spacy
-        extractor.load_document(input=doc, language='en')
+            # load the content of the document, here document is expected to be in raw
+            # format (i.e. a simple text file) and preprocessing is carried out using spacy
+            extractor.load_document(input=doc, language='en')
 
-        # keyphrase candidate selection, in the case of TopicRank: sequences of nouns
-        # and adjectives (i.e. `(Noun|Adj)*`)
-        extractor.candidate_selection()
+            # keyphrase candidate selection, in the case of TopicRank: sequences of nouns
+            # and adjectives (i.e. `(Noun|Adj)*`)
+            extractor.candidate_selection()
 
-        # candidate weighting, in the case of TopicRank: using a random walk algorithm
-        extractor.candidate_weighting()
+            # candidate weighting, in the case of TopicRank: using a random walk algorithm
+            extractor.candidate_weighting()
 
-        # N-best selection, keyphrases contains the 10 highest scored candidates as
-        # (keyphrase, score) tuples
-        keyphrases = extractor.get_n_best(n=10)
+            # N-best selection, keyphrases contains the 10 highest scored candidates as
+            # (keyphrase, score) tuples
+            keyphrases = extractor.get_n_best(n=10)
 
-        return [kw for kw, sc in keyphrases]
+            return [kw for kw, sc in keyphrases]
+        except:
+            return []
 
     def __getProfanityValue__(self, doc):
-        return predict_prob([doc])[0]
+        try:
+            return predict_prob([doc])[0]
+        except:
+            return None
 
     def __getCitiesInDoc__(self, doc):
-        cities_in_article = keyword_processor.extract_keywords(doc)
-        return list(set(cities_in_article))
+        try: 
+            cities_in_article = keyword_processor.extract_keywords(doc)
+            return list(set(cities_in_article))
+        except:
+            return []
 
     def __getEmotions__(self, doc):
-        emos = te.get_emotion(doc)
-        mostCommonEmos = Counter(emos).most_common(2)
-        return [e for e, prob in mostCommonEmos]
+        try:
+            emos = te.get_emotion(doc)
+            mostCommonEmos = Counter(emos).most_common(2)
+            return [e for e, prob in mostCommonEmos]
+        except:
+            return []
---
+++ modules.py
@@ -0,0 +1,7 @@
+import nltk
+import os
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+os.system('python -m spacy download en_core_web_sm')
